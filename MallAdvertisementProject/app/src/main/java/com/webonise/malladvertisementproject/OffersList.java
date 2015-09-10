24b8b64908ae6dcbc0485a3c171c17916f231d72
shubhamdani <shubhamdani-weboniselab>
2015-09-10 13:48:48 +0530

    Material design Color blue done with new icon

--- MallAdvertisementProject/app/src/main/java/com/webonise/malladvertisementproject/OffersList.java
+++ MallAdvertisementProject/app/src/main/java/com/webonise/malladvertisementproject/OffersList.java
@@ -1,17 +1,28 @@
 package com.webonise.malladvertisementproject;
 
-import android.app.Activity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class OffersList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setToolBar();
+    }
+
+    private void setToolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setTitle(getTitle());
+
     }
 
 
